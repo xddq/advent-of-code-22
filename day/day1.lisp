dbcf857b14a3e16e79248fbb29a6eb718a901bed
Pierre Dahmani <git@pierrefhapp.dev>
2022-12-03 11:57:41 +0100

    day1 version3: - used :key designator available in reduce to parse input to integer   - reduce docu: http://www.lispworks.com/documentation/lw50/CLHS/Body/f_reduce.htm#reduce - used #'> instead of unnecessary lambda - used :end available in reduce to end reducing after 3 entries (calculate the first three entries) - grouped closing parens in the same line :)))

--- day/day1.lisp
+++ day/day1.lisp
@@ -2,37 +2,25 @@
 ;; common lisp!
 
 (defun load-deps ()
-  "Loads libraries to be available in code"
-  (require "asdf")
-  (load "~/quicklisp/setup.lisp")
+  "Loads libraries required for the rest of the code"
   (ql:quickload :alexandria))
 
 (defun read-input ()
-  (uiop:read-file-lines "./input/day1"))
-
-(defun parse-input (lines)
-  "Based on the given lines, returns a list of lists where each inner list
-  contains all calories for a given elf"
-  ; Creates a list of lists with each inner list containing all calories per elf
-  (let ((elves-calories (split-sequence:split-sequence "" lines
-                                                       :test #'equal
-                                                       :remove-empty-subseqs t)))
-    ; converts string values to int for each inner list
-    (mapcar (lambda (calories) (mapcar #'parse-integer calories)) elves-calories)))
+  "Reads input from file and creates a list of lists with each inner list
+  containing all calories per elf."
+  (let ((lines (uiop:read-file-lines "./input/day1")))
+     (split-sequence:split-sequence "" lines :test #'equal
+                                             :remove-empty-subseqs t)))
 
 (defun main ()
   (load-deps)
-  (let ((elves-calories (parse-input (read-input))))
-    (let ((calories-sum (mapcar
-                          (lambda (calories-per-elf) (reduce #'+ calories-per-elf))
-                          elves-calories)))
-      (let ((calories-sorted (sort calories-sum (lambda (a b) (> a b)))))
-        ; Returns the highest calories (part1) and the sum of the highest three
-        ; (part2)
-        (values (first calories-sorted) (reduce #'+ (subseq calories-sorted 0 3)))
-      )
-    )
-  )
-)
+  (let ((elves-calories (read-input)))
+    ; calculates sum for all calories per elf
+    (let ((calories-sum (mapcar (lambda (calories-per-elf)
+                          (reduce #'+ calories-per-elf :key #'parse-integer))
+                        elves-calories)))
+      (let ((calories-sorted (sort calories-sum #'>)))
+        ; Returns highest calories (part1) and the sum of the highest three (part2)
+        (values (first calories-sorted) (reduce #'+ calories-sorted :end 3))))))
 
 (main)
