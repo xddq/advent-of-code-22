5c207b6657f79029729b3e421b3e4193c0212fb3
Pierre Dahmani <git@pierrefhapp.dev>
2022-12-03 10:32:57 +0100

    day1 version2: - using let instead of defparameter - creating/using functions for reading input and parsing it - using reduce #'+ instead of creating own function

--- day/day1.lisp
+++ day/day1.lisp
@@ -1,29 +1,38 @@
-;; CAREFUL, your eyes might hurt after this. It iss the first time I am touching
+;; CAREFUL, your eyes might hurt after this. It is the first time I am touching
 ;; common lisp!
 
-(defun main ()
-  ; loads libraries into common lisp using quickload
+(defun load-deps ()
+  "Loads libraries to be available in code"
   (require "asdf")
   (load "~/quicklisp/setup.lisp")
-  (ql:quickload "iterate")
-  (ql:quickload "for")
-  (ql:quickload :alexandria)
-  (defparameter input (uiop:read-file-lines "./input/day1"))
-  (defparameter elves-calories (split-sequence:split-sequence "" input
-                                                            :test #'equal
-                                                            :remove-empty-subseqs t))
-  ; converts list of list of strings to list of list of integers
-  (defparameter elves-calories-int (mapcar (lambda (elves) (mapcar (lambda (elf-calories) (parse-integer elf-calories)) elves)) elves-calories))
-  ; calculates sum for each list in the list of lists
-  (defparameter calories-sum  (mapcar (lambda (a) (reduce (lambda (prev curr) (+ prev curr)) a)) elves-calories-int))
-  ; sorts largest first
-  (defparameter sorted-calories (sort calories-sum (lambda (a b) (> a b))))
-  (first sorted-calories)
-  (defparameter largest-three (subseq sorted-calories 0 3))
-  (defparameter total-of-largest-three ( reduce (lambda (prev curr) (+ prev curr)) largest-three))
-  ; returns multiple values from function to get both solutions
-  (values (first sorted-calories) total-of-largest-three)
-  )
+  (ql:quickload :alexandria))
 
+(defun read-input ()
+  (uiop:read-file-lines "./input/day1"))
+
+(defun parse-input (lines)
+  "Based on the given lines, returns a list of lists where each inner list
+  contains all calories for a given elf"
+  ; Creates a list of lists with each inner list containing all calories per elf
+  (let ((elves-calories (split-sequence:split-sequence "" lines
+                                                       :test #'equal
+                                                       :remove-empty-subseqs t)))
+    ; converts string values to int for each inner list
+    (mapcar (lambda (calories) (mapcar #'parse-integer calories)) elves-calories)))
+
+(defun main ()
+  (load-deps)
+  (let ((elves-calories (parse-input (read-input))))
+    (let ((calories-sum (mapcar
+                          (lambda (calories-per-elf) (reduce #'+ calories-per-elf))
+                          elves-calories)))
+      (let ((calories-sorted (sort calories-sum (lambda (a b) (> a b)))))
+        ; Returns the highest calories (part1) and the sum of the highest three
+        ; (part2)
+        (values (first calories-sorted) (reduce #'+ (subseq calories-sorted 0 3)))
+      )
+    )
+  )
+)
 
 (main)
